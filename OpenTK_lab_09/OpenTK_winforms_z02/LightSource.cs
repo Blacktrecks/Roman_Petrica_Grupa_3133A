1e619cd7074fe116c8d60c6654b4df08df39ef35
Blacktrecks <107044395+Blacktrecks@users.noreply.github.com>
2023-12-16 11:19:45 +0200

    Add lab9

---
+++ OpenTK_lab_09/OpenTK_winforms_z02/LightSource.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_winforms_z02
+{
+    public class LightSource
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Ambient { get; set; }
+        public Vector3 Diffuse { get; set; }
+        public Vector3 Specular { get; set; }
+
+        public LightSource(Vector3 position, Vector3 ambient, Vector3 diffuse, Vector3 specular)
+        {
+            Position = position;
+            Ambient = ambient;
+            Diffuse = diffuse;
+            Specular = specular;
+        }
+    }
+}
